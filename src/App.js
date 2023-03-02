bdbd5863a31d52b9020ee273ffdb6b65a58d6107
Aditya Ghadge <adityaghadge360@gmail.com>
2023-03-03 00:57:59 +0530

    Create faucet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import {
+  Connection,
+  PublicKey,
+  LAMPORTS_PER_SOL,
+  clusterApiUrl,
+} from "@solana/web3.js";
 
 function App() {
+  const [address, setAddress] = useState("");
+  const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+
+  async function airDropSol() {
+    try {
+      const ADDRESS = new PublicKey(address);
+
+      // console.log(PublicKey.isOnCurve(ADDRESS));
+
+      const signature = await connection.requestAirdrop(
+        ADDRESS,
+        LAMPORTS_PER_SOL
+      );
+
+      const { blockhash, lastValidBlockHeight } =
+        await connection.getLatestBlockhash();
+
+      // 3 - Confirm transaction success
+      await connection.confirmTransaction(
+        {
+          blockhash,
+          lastValidBlockHeight,
+          signature,
+        },
+        "finalized"
+      );
+
+      // 4 - Log results
+      console.log(
+        `Tx Complete: https://explorer.solana.com/tx/${signature}?cluster=devnet`
+      );
+    } catch (error) {
+      console.log(error);
+      return;
+    }
+
+    setAddress("");
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <h1>
+          <span className="span"> Solana Devnet Faucet:</span> Get Free SOL for
+          Testing Your Dapps
+        </h1>
+        <label htmlFor="address">
+          Enter Your Wallet Address Below <span className="symbol"> 👇️</span>{" "}
+        </label>
+        <input
+          type="text"
+          id="address"
+          value={address}
+          autoComplete="off"
+          placeholder="9v9XWzB8EQVJNpPfMvzbNgnWZkHgSsT1T1KTzv9XpT8n"
+          onChange={(e) => setAddress(e.target.value)}
+          required
+        />
+        <button onClick={airDropSol}>Send Request</button>
+      </div>
+    </>
   );
 }
-
 export default App;
